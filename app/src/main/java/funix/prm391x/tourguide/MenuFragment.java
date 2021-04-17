d22c9a84e13eaf5fce79164644627637c15b0623
Luan Nguyen <nguyenngocluan.298@gmail.com>
2021-04-17 15:48:35 +0700

    Processing 1.0

--- app/src/main/java/funix/prm391x/tourguide/MenuFragment.java
+++ app/src/main/java/funix/prm391x/tourguide/MenuFragment.java
@@ -3,10 +3,15 @@ package funix.prm391x.tourguide;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,14 +20,7 @@ import android.view.ViewGroup;
  */
 public class MenuFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private ImageButton mHotelButton;
 
     public MenuFragment() {
         // Required empty public constructor
@@ -40,8 +38,6 @@ public class MenuFragment extends Fragment {
     public static MenuFragment newInstance(String param1, String param2) {
         MenuFragment fragment = new MenuFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -49,16 +45,29 @@ public class MenuFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu, container, false);
+        View layout = inflater.inflate(R.layout.fragment_menu, container, false);
+        mHotelButton = layout.findViewById(R.id.main_activity_imb_hotel);
+        mHotelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "clicked", Toast.LENGTH_SHORT).show();
+                Fragment hotelFragment = new Fragment();
+                FragmentManager fragmentManager = getFragmentManager();
+                fragmentManager.beginTransaction().replace(R.id.frame_layout, hotelFragment)
+                        .commit();
+            }
+        });
+
+
+        return layout;
+
+
     }
 }
